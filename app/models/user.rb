4e18f30c000dde2260f65b6f41b2649b22643e38
Carlos Henrique <hencarlosdv@gmail.com>
2025-05-12 20:55:44 -0300

    fix: using enum properly

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  enum role: { trainee: 0, trainer: 1 }
+  enum :role, { trainee: 0, trainer: 1 }
 end
