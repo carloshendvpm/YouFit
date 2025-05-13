9650950cebe04bc17fba0de6027a148f5fa75620
Carlos Henrique <hencarlosdv@gmail.com>
2025-05-12 22:46:19 -0300

    feat: add relation to workout_days

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,4 +1,5 @@
 class Workout < ApplicationRecord
   belongs_to :created_by, class_name: "User"
   belongs_to :assigned_to, class_name: "User", optional: true
+  has_many :workout_days, dependent: :destroy
 end
