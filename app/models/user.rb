936cb40c22f3d061a04ae51c143cd7524efca74d
Carlos Henrique <hencarlosdv@gmail.com>
2025-05-12 22:23:53 -0300

    feat: add relations user/trainees

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   enum :role, { trainee: 0, trainer: 1 }
+  has_many :trainee_relationships, class_name: "TraineeTrainer", foreign_key: :trainee_id
+  has_many :trainees, through: :trainee_relationships, source: :trainee
+  has_one :trainer_relationships, class_name: "TraineeTrainer", foreign_key: :trainer_id
+  has_one :trainer, through: :trainer_relationships, source: :trainer
 end
